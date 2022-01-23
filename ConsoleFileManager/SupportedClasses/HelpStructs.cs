d05cd5c3c26e7d6bd32f1f912da1720be475f630
SergeySvist <sergeysvist557@gmail.com>
2022-01-23 13:35:24 +0200

    add: helping structs

---
+++ ConsoleFileManager/SupportedClasses/HelpStructs.cs
@@ -0,0 +1,26 @@
+﻿namespace CFM;
+
+
+struct Point
+{
+    int X { get; set; }
+    int Y { get; set; }
+
+    public Point(int x,int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+struct Size
+{
+    int H { get; set; }
+    int W { get; set; }
+
+    public Size(int height, int width)
+    {
+        H = height;
+        W = width;
+    }
+}
